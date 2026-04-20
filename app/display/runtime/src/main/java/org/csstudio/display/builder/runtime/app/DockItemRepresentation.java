8d92bc3b6272264af186cc36b06da1d0f6f8a73b
Rebecca Williams <rjw@observatorysciences.co.uk>
2026-04-20 11:04:58 +0100

    Set smaller stage margins for standalone windows

--- app/display/runtime/src/main/java/org/csstudio/display/builder/runtime/app/DockItemRepresentation.java
+++ app/display/runtime/src/main/java/org/csstudio/display/builder/runtime/app/DockItemRepresentation.java
@@ -58,7 +58,6 @@ public class DockItemRepresentation extends JFXRepresentation
 
         // Open new Stage
         final Stage new_stage = new Stage();
-
         // Configure for docking, i.e. with DockPane
         DockStage.configureStage(new_stage, new Geometry(null), model.isStandalone());
 
@@ -84,8 +83,16 @@ public class DockItemRepresentation extends JFXRepresentation
         // Size needs to account for the border and toolbar.
         // Using fixed numbers, exact size of border and toolbar unknown
         // at this time in the code
-        new_stage.setWidth(model.propWidth().getValue() + 18);
-        new_stage.setHeight(model.propHeight().getValue() + 105);
+        int width_margin = 18;
+        int height_margin = 105;
+        // Don't need to include space for toolbar in standalone
+        if (model.isStandalone())
+        {
+            width_margin = 9;
+            height_margin = 40;
+        }
+        new_stage.setWidth(model.propWidth().getValue() + width_margin);
+        new_stage.setHeight(model.propHeight().getValue() + height_margin);
 
         new_stage.show();
 
